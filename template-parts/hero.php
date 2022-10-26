8dd03d6e8adad56a3c098a1177a419e9b3662960
unknown <dhanna@alphaworks.tech>
2022-10-26 16:44:18 -0700

    Added ACF to Hero Section

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,11 +1,11 @@
 
 <section class="HeroSection">
-<img src="http://thesetuppractice01.local/wp-content/uploads/2022/10/GLADIUS-Drone-01.jpg" alt="" width="" height="">
+<img src="<?php the_field('hero_image');?>" alt="" width="" height="">
 <div class="heroshape"></div>
 <div class="herotextbox">
-    <h3>About Underwater Exploration</h3>
-    <h1>Autonomous Underwater Vehicles</h1>
-    <p>In order to explore the Last Frontier safely, we highly suggest utilizing our new Autonomous Underwater Vehicles. To see our wide selection, please press the button below.</p>
-    <button>Learn More</button>
+    <h3><?php the_field('hero_title1');?></h3>
+    <h1><?php the_field('hero_title2');?></h1>
+    <p><?php the_field('hero_title3');?></p>
+    <a href="<?php the_field('hero_button_url');?>"><button><?php the_field('hero_button_text');?></button></a>
 </div>
 </section>
